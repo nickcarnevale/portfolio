ef107e5477f0e1871a1ccf9a2dd117b703f3457a
Nicholas Carnevale <nickcarnevale2@gmail.com>
2025-11-18 23:47:29 -0500

    added text type animation

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,18 @@
+import TextType from "@/animations/TextType";
+
 export default function Home() {
   return (
     <main className="h-full w-full flex flex-col items-center justify-center gap-6 text-white">
-      <h1 className="text-6xl font-bold">Nicholas Carnevale</h1>
-      <p className="text-xl opacity-90">Full Stack Engineer</p>
+      <TextType
+        text={["Nicholas Carnevale"]}     // typing text
+        typingSpeed={60}                  // adjust typing speed
+        pauseDuration={2000}              // pause before deleting or stopping
+        showCursor={true}                 // blinking cursor
+        cursorCharacter="|"               
+        className="text-6xl font-bold"    // same styling as your <h1>
+        loop={false}                      // only type once
+      />
+      {/* <p className="text-xl opacity-90">Full Stack Engineer</p> */}
     </main>
   );
 }
